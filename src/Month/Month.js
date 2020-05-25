f64d91301f99ba331e0bad3dfe88a80830ada310
chosxxx <chosxxx@gmail.com>
2020-05-25 09:03:48 -0500

    Update Month.js
    Removed console.log

--- src/Month/Month.js
+++ src/Month/Month.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Month(props) {
     var date = new Date(props.year, props.month, 1);
-    console.log(props.year, props.month);
     return (
         <div className="Month-header-element">
             <p>{date.toLocaleDateString("default", {month: "long"})} {props.year}</p>
